699727ca31a200cd819fcc534f5e6b9d0cfe3f96
Amir Khodabande <amirdp8102@gmail.com>
2021-01-05 15:48:07 +0330

    Add home component

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <div class="container">
+        <section class="card col-8 m-auto">
+            <header class="card-header font-weight-bolder">SPA with Laravel and VueJs</header>
+            <main class="card-body">{{welcome}}</main>
+        </section>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "Home",
+    computed: {
+        welcome() {
+            return this.$store.getters.welcome
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>

c5f81092a187e50570c740a0fc1c8a09063e1fad
Amir Khodabande <amirdp8102@gmail.com>
2021-01-07 13:11:35 +0330

    Refactor and cleanup app.js  and move some initialize codes to the general.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,3 @@
-
-
 require('./bootstrap');
 
 import Vue from 'vue'
@@ -8,6 +6,7 @@ import Vuex from 'vuex'
 import {routes} from './routes'
 import StoreData from './store'
 import MainApp from "./components/MainApp"
+import {initialize} from './helpers/general'
 
 Vue.use(VueRouter);
 Vue.use(Vuex);
@@ -19,20 +18,7 @@ const router = new VueRouter({
     mode: "history"
 });
 
-//Atuh check
-router.beforeEach((to, from, next) => {
-    const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
-    const currentUser = store.state.currentUser;
-
-    if(requiresAuth && !currentUser) {
-        next('/login');
-    } else if(to.path == '/login' && currentUser) {
-        next('/');
-    } else {
-        next();
-    }
-})
-
+initialize(store, router)
 
 const app = new Vue({
     el: '#app',

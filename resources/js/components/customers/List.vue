d2116217404c42765fe37858e634cb5312fa450b
Amir Khodabande <amirdp8102@gmail.com>
2021-01-08 17:16:38 +0330

    Add List.vue

--- resources/js/components/customers/List.vue
+++ resources/js/components/customers/List.vue
@@ -1,9 +1,50 @@
 <template>
+    <div>
+        <div class="text-right mb-1">
+            <router-link to="/customers/new" class="btn btn-sm btn-primary">Add New Customer</router-link>
+        </div>
 
+        <table class="table">
+            <thead>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Actions</th>
+            </thead>
+            <tbody>
+            <template v-if="!customers.length">
+                <tr>
+                    <td colspan="4" class="text-center">No Customers Available</td>
+                </tr>
+            </template>
+
+            <template v-else>
+                <tr v-for="customer in customers" :key="customer.id">
+                    <td>{{ customer.name }}</td>
+                    <td>{{ customer.email }}</td>
+                    <td>{{ customer.phone }}</td>
+                    <td>
+                        <router-link to="`/customers/${customer.id}`">View</router-link>
+                    </td>
+                </tr>
+            </template>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
 export default {
+    name: 'list',
 
+    mounted() {
+        this.$store.dispatch('getCustomers');
+    },
+
+    computed: {
+        customers() {
+            return this.$store.getters.customers;
+        }
+    }
 }
 </script>

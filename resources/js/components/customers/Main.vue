8c28820f41292fb3b98ce4bbd0c477b986df539b
Amir Khodabande <amirdp8102@gmail.com>
2021-01-08 17:11:44 +0330

    Add Main.vue to show all of the Customers pages inside the main page

--- resources/js/components/customers/Main.vue
+++ resources/js/components/customers/Main.vue
@@ -1,9 +1,25 @@
 <template>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <section class="card card-default">
 
+                <header class="card-header">
+                    Customers
+                </header>
+
+                <main class="card-body">
+                    <router-view></router-view>
+                </main>
+
+            </section>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
 export default {
-
+    name: 'customers-main'
 }
 </script>

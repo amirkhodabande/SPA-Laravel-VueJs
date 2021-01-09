ccbe71146f7ab0c651dc8971edc98a64f37f6fa5
Amir Khodabande <amirdp8102@gmail.com>
2021-01-09 14:03:56 +0330

    Fix token error

--- resources/js/helpers/auth.js
+++ resources/js/helpers/auth.js
@@ -1,9 +1,11 @@
+import { setAuthorization } from "./general";
 
 export function login(credentials) {
     return new Promise((res, rej) => {
 
         axios.post('/api/auth/login', credentials)
             .then((response) => {
+                setAuthorization(response.data.access_token);
                 res(response.data);
             })
             .catch((err) => {
--- resources/js/helpers/general.js
+++ resources/js/helpers/general.js
@@ -24,8 +24,12 @@ export function initialize(store, router) {
     });
 
     //Add default header to all axios requests
-    if(store.getters.currentUser) {
-        axios.defaults.headers.common['Authorization'] = `Bearer ${store.getters.currentUser.token}`
+    if (store.getters.currentUser) {
+        setAuthorization(store.getters.currentUser.token);
     }
 
+}
+
+export function setAuthorization(token) {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`
 }
